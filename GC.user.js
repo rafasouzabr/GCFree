c22dec78446b6f8677d79c2ba3d190e87805a51d
Rafael <rafasouza_bv@yahoo.com.br>
2020-07-17 22:30:31 -0300

    first commit

---
+++ GC.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         GC
+// Atenção:      Caso algum site não funcione logo após a instalação, limpe o cache do navegador.
+// @grant        GM_webRequest
+// @grant        GM_xmlhttpRequest
+// @match        *://gamersclub.com.br/*
+// @run-at       document-end
+// @noframes
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    ISSUBSCRIBER = true; 
+
+})();
