b63a2f812e99025df327a483645ae4fa630c1cff
Rafael <rafasouza_bv@yahoo.com.br>
2020-07-17 22:38:38 -0300

    first commit

--- GC.user.js
+++ GC.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
-// @name         GC
+// @name         GC Premium Free
+// @version      0.1
 // Atenção:      Caso algum site não funcione logo após a instalação, limpe o cache do navegador.
 // @grant        GM_webRequest
 // @grant        GM_xmlhttpRequest
 // @match        *://gamersclub.com.br/*
+// @downloadURL  https://gdplus.github.io/releases/gdplus.user.js
 // @run-at       document-end
 // @noframes
 // ==/UserScript==
