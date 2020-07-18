6a8de4edaab24ba05333593e9f2f096ef928bef8
Rafael <rafasouza_bv@yahoo.com.br>
2020-07-18 15:56:21 -0300

    add auto pre-ready and ready

--- GC.user.js
+++ GC.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         GC Premium Free
 // @description  Adiciona funcionalidade para Gamers Club
-// @version      0.3
+// @version      0.2
 // @author       Rafael
 // @grant        GM_webRequest
 // @grant        GM_xmlhttpRequest
 // @match        *://gamersclub.com.br/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @downloadURL  https://github.com/rafasouzabr/GCFree/raw/master/GC.user.js
 // @updateURL    https://github.com/rafasouzabr/GCFree/raw/master/GC.user.js
 // @run-at       document-end
@@ -17,14 +18,14 @@
     ISSUBSCRIBER = true;
 
 	setInterval(function(){
-         if ( document.getElementById('gameModalReadyBalls').style.display != "none" && $("#clock").text() <> "") {
+         if ( $("#rankedModals .game-modal-footer #gameModalReadyBalls").css("display") != "none" && $("#clock").text() != "" && $("#rankedModals #gameModalReady #gameModalReadyBtn button.game-modal-ready-button").length > 0) {
              lobby.setGameReady();
              console.log('Ready Pronto');
         }
-         if ( document.getElementsByClassName('preready-block').length > 0 ) {
+         if ( $("#lobbyModal .preready-block").length > 0 && $("#lobbyModal .preready-timer").text() != "" && $("#lobbyModal #setPlayerReady").length > 0 && $("#lobbyModal .preready-player-ready").length == 0) {
              setPreReady();
-             console.log('Pre-ready pronto');
+             console.log('Pré-Ready pronto');
          }
-    },500); //change 500 to the time in MS to wait before each run.	
+    },500);
 
 })();
