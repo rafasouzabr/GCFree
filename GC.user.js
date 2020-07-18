8e443f8048680a06ebde23f83e4581f81b329fdb
Rafael <rafasouza_bv@yahoo.com.br>
2020-07-17 23:11:10 -0300

    first commit

--- GC.user.js
+++ GC.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         GC Premium Free
-// @version      0.2
+// @version      0.1
 // Atenção:      Caso algum site não funcione logo após a instalação, limpe o cache do navegador.
 // @grant        GM_webRequest
 // @grant        GM_xmlhttpRequest
